dbb6733de1cf7d2b063a71e853b6a7d8103f0541
Matt Sell <mwsell@gmail.com>
2016-05-08 17:17:52 -0500

    pass props

--- app/app.js
+++ app/app.js
@@ -3,10 +3,11 @@ var ReactDOM = require('react-dom');
 
 var Hello = React.createClass({
   render: function () {
+    console.log(this.props);
     return (
-      <div>Hello World!</div>
+      <div>Hello {this.props.name}</div>
     )
   }
 });
 
-ReactDOM.render(<Hello />, document.getElementById('app'));
+ReactDOM.render(<Hello name="Matt" />, document.getElementById('app'));
